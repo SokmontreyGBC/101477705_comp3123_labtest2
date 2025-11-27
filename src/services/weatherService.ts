e9db051000e4413ec5aa24ab73c7ff8a5a211a64
Sokmontrey <sythatsokmontrey@gmail.com>
2025-11-27 12:06:33 -0500

    chore: Remove React SVG asset.

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { WeatherData } from '@/components/WeatherCard';
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
-export const fetchWeatherData = async (city) => {
+export const fetchWeatherData = async (city: string): Promise<WeatherData> => {
     try {
-        const response = await axios.get(BASE_URL, {
+        const response = await axios.get<WeatherData>(BASE_URL, {
             params: {
                 q: city,
                 appid: API_KEY,
